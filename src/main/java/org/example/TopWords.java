fe72e72d90b3073b1d4ad6303bd267bc51d72187
Cesar Forthomme <cesar.forthomme@appliedtechnology.se>
2023-05-04 07:14:36 +0200

    feat(TopWords): Implemented Hashmap and for loop

--- src/main/java/org/example/TopWords.java
+++ src/main/java/org/example/TopWords.java
@@ -1,11 +1,26 @@
 package org.example;
 
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 public class TopWords {
 
   public static List<String> top3(String s) {
     // Your code here
+    Map<String, Integer> wordFrequency = new HashMap<String, Integer>();
+
+    String[] words = s.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+");
+
+    for (String word : words) {
+      if (wordFrequency.containsKey(word)) {
+        int count = wordFrequency.get(word);
+        wordFrequency.put(word, count + 1);
+      } else {
+        wordFrequency.put(word, 1);
+      }
+    }
+
     return null;
   }
 }
