ef6e2a4b95e1409485c1dbe1d0cd2cdc0eb62f24
Cesar Forthomme <cesar.forthomme@appliedtechnology.se>
2023-05-04 08:37:31 +0200

    Streamed the map

--- src/main/java/org/example/TopWords.java
+++ src/main/java/org/example/TopWords.java
@@ -1,10 +1,13 @@
 package org.example;
 
-import java.util.List;
-import java.util.Map;
-import java.util.HashMap;
+import java.util.*;
+import java.util.Collections;
 
 public class TopWords {
+
+  public static void main(String[] args) {
+
+  }
 
   public static List<String> top3(String s) {
     // Your code here
@@ -20,6 +23,23 @@ public class TopWords {
         wordFrequency.put(word, 1);
       }
     }
+    /*
+    // sort by frequency count and store in a string array
+    List<Map.Entry<String, Integer>> sortedList = new ArrayList<>(wordFrequency.entrySet());
+    String[] top3 =  sortedList
+            .stream()
+            .sorted((Map.Entry<String, Integer> e1, Map.Entry<String, Integer> e2) ->  e2.getValue().compareTo(e1.getValue()))
+            .collect()
+
+     */
+    String[] sortedWords = new String[sortedList.size()];
+    for (int i = 0; i < sortedList.size(); i++) {
+      sortedWords[i] = sortedList.get(i).getKey();
+    }
+
+    System.out.println(Arrays.toString(sortedWords));
+
+
 
     return null;
   }
